76d6386e3bbf2f947bb2121c050a8b025f12caed
Keerthisree Raghu <raghu.keerthisree@gmail.com>
2020-12-31 23:59:34 +0530

    executes CRD operations with functional requirements

--- create-read-delete.js
+++ create-read-delete.js
@@ -4,17 +4,21 @@ const filepath = 'datastore.json';
 
 var entries = new Map([]);
 
-createData = (key, value, timeToLive = 0) => {
+// Creates and writes key-value pair data into a json file with optional time-to-live property
+createData = (key, value, expiryTime = 0) => {
     const valueSize = Buffer.byteLength(JSON.stringify(value));
-    var expiryTime = timeToLive * 1000;
+    var timeToLive = expiryTime * 1000;
     if (fs.existsSync(filepath)) {
-        fs.readFile(filepath, 'utf8', (err) => {
+        fs.readFile(filepath, 'utf8', (err, fileData) => {
+            const fileSize = Buffer.byteLength(JSON.stringify(fileData));
             if (err) {
                 console.log(err);
             } else if (entries.has(key)) {
                 console.log("Key already exists.");
+            } else if (fileSize >= 1000000000) {
+                console.log("File size limit breached. (Max limit: 1 GB)");
             } else if (typeof key === 'string' && key.length <= 32 && valueSize <= 16000) {
-                val = {value, expiryTime};
+                val = { value, timeToLive };
                 entries.set(key, val);
                 jsonData = JSON.stringify(Object.fromEntries(entries), null, 2);
                 fs.writeFileSync(filepath, jsonData, err => {
@@ -26,25 +30,27 @@ createData = (key, value, timeToLive = 0) => {
         });
     }
     else {
-        val = {value, expiryTime};
+        val = { value, timeToLive };
         entries.set(key, val);
         jsonData = JSON.stringify(Object.fromEntries(entries), null, 2);
         fs.writeFileSync(filepath, jsonData, err => {
             if (err) throw err;
         });
     }
-    if (expiryTime > 0) {
-        setTimeout(deleteData, expiryTime, key);
+    // Delete the data from the datastore once the expiry time has lapsed
+    if (timeToLive > 0) {
+        setTimeout(deleteData, timeToLive, key);
     }
 }
 
+// Reads data from the file and returns the value for the given key in JSON object form
 readData = (key) => {
     if (fs.existsSync(filepath)) {
         setTimeout(() => fs.readFile(filepath, (err, fileData) => {
-            const object = JSON.parse(fileData);
+            const parsedFileData = JSON.parse(fileData);
             if (err) throw err;
-            else if (object[key] && entries.has(key)) {
-                console.log(object[key]);
+            else if (parsedFileData[key] && entries.has(key)) {
+                console.log(parsedFileData[key]);
             }
             else {
                 console.log(`"${key}" key does not exist or has expired.`);
@@ -56,13 +62,14 @@ readData = (key) => {
     }
 }
 
+// Deletes the key-value pair data from the data store for the given key
 deleteData = (key) => {
     if (fs.existsSync(filepath)) {
         setTimeout(() => fs.readFile(filepath, (err, fileData) => {
-            const object = JSON.parse(fileData);
+            const parsedFileData = JSON.parse(fileData);
             if (err) throw err;
-            else if (object[key] && entries.has(key)) {
-                delete object[key];
+            else if (parsedFileData[key] && entries.has(key)) {
+                delete parsedFileData[key];
                 entries.delete(key);
                 fs.writeFileSync(filepath, JSON.stringify(Object.fromEntries(entries), null, 2));
             }
