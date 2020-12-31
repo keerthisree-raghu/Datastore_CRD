d12c8f97b3296ac9b704dbcc0fcc6585dbb20252
Keerthisree Raghu <raghu.keerthisree@gmail.com>
2020-12-31 23:55:23 +0530

    runs unit tests for crd operations

--- test/datastore.test.js
+++ test/datastore.test.js
@@ -8,49 +8,46 @@ const datastore = require('../create-read-delete');
 
 const filepath = 'datastore.json';
 var entries = new Map([]);
-var object;
+var parsedFileData;
 
 describe('Data store tests', () => {
+    // Checks existence of datastore file
     describe('File creation test', () => {
         it('JSON file exists', () => {
             expect(fs.existsSync(filepath)).to.be.true;
         });
     });
 
+    // Before executing each test, read the contents of the file and parse the data
     beforeEach(function (done) {
+        // Parse and store the data in a variable if the file already exists
         if (fs.existsSync(filepath)) {
-            fs.readFile(filepath, 'utf-8', (err, data) => {
+            fs.readFile(filepath, 'utf-8', (err, fileData) => {
                 if (err) throw err;
-                object = JSON.parse(data);
+                parsedFileData = JSON.parse(fileData);
                 done();
             });
         }
+        // Create a new file if the file does not exist and write a key-value pair into the file
         else {
-            entries.set("Foo", { name: "Bar", age: 25 })
+            entries.set("Foo", { value: "Bar", expiryTime: 0 })
             jsonData = JSON.stringify(Object.fromEntries(entries), null, 2);
             fs.writeFile(filepath, jsonData, (err) => {
                 if (err) throw err;
             });
-            fs.readFile(filepath, 'utf-8', (err, data) => {
+            // Read and parse the data after it has been written
+            fs.readFile(filepath, 'utf-8', (err, fileData) => {
                 if (err) throw err;
-                object = JSON.parse(data);
+                parsedFileData = JSON.parse(fileData);
                 done();
             });
         }
     });
 
-    describe('readData() tests', () => {
-        it('should pass if file contents are of JSON object format', () => {
-            assert.isObject(object, 'is JSON object');
-        });
-        it('should pass if the value of a given key is a JSON object', () => {
-            assert.isObject(object["Foo"], 'Value is a JSON object')
-        });
-    });
-
+    // Create Operations Tests
     describe('createData() tests', () => {
         it('should pass if the key of type string', () => {
-            var key = Object.keys(object)[0];
+            var key = Object.keys(parsedFileData)[0];
             assert.isString(key, 'Key is a string');
         });
         it('should fail if the key is not a string', () => {
@@ -58,21 +55,32 @@ describe('Data store tests', () => {
             assert.isNotString(key, 'Key is a not a string');
         });
         it('should pass if the key length is less than or equal to 32 characters', () => {
-            var key = Object.keys(object)[0];
+            var key = Object.keys(parsedFileData)[0];
             assert.isAtMost(key.length, 32, 'Key is less than or equal to 32 characters');
         });
         it('should pass if the size of the value is less than 16 KB', () => {
-            const valueSize = Buffer.byteLength(JSON.stringify(Object.values(object)));
+            const valueSize = Buffer.byteLength(JSON.stringify(Object.values(parsedFileData)));
             assert.isAtMost(valueSize, 16000, 'Value is less than or equal to 16 KB');
         });
         it('should fail if key exists already', () => {
-            assert.hasAnyKeys(object, ['Foo', 'Baz', 'Quux'], 'Key already exists')
+            assert.hasAnyKeys(parsedFileData, ['Foo', 'Baz', 'Quux'], 'Key already exists')
         });
     });
 
+    // Read Operation Tests
+    describe('readData() tests', () => {
+        it('should pass if file contents are of JSON object format', () => {
+            assert.isObject(parsedFileData, 'is JSON object');
+        });
+        it('should pass if the value of a given key is a JSON object', () => {
+            assert.isObject(parsedFileData["Foo"], 'Value is a JSON object')
+        });
+    });
+    
+    // Delete Operations Test
     describe('deleteData() tests', () => {
         it('should pass if the key exists in the datastore', () => {
-            assert.containsAllKeys(object, ['Foo'], 'Key can be deleted')
+            assert.containsAllKeys(parsedFileData, ['Foo'], 'Key can be deleted')
         });
     });
 });
