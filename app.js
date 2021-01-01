22c1a0aef12261dfa42072ecea543dfaa36deca1
Keerthisree Raghu <raghu.keerthisree@gmail.com>
2021-01-01 11:24:59 +0530

    executes create, read, delete operations

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-var datastore = require('./create-read-delete');
+var datastore = require("./create-read-delete");
 
 // Create data without TTL
-datastore.createData("Foo", "Bar");
+datastore.createData("Foo", "Bar", "data.json");
 
-// Read data
+// Read and display the value for the key "Foo" if it exists in the datastore
 datastore.readData("Foo");
 
-// Create data with TTL (1 second)
+// Create data with TTL (5 seconds)
 datastore.createData("Baz", "Qux", 5);
 
-// Read data before expiry
+// Read data for the key "Baz" before expiry
 datastore.readData("Baz");
 
-// Read expired data (should fail)
+// Read data for key "Baz" after expiry (should fail)
 setTimeout(() => datastore.readData("Baz"), 6000);
 
 // Create data without TTL
 datastore.createData("Quux", "Quuz");
 
-// Delete data using key
+// Delete data using key "Quux"
 datastore.deleteData("Quux");
 
-// Read deleted data (should fail)
+// Read deleted data for the key "Quux" (should fail)
 datastore.readData("Quux");
 
 
